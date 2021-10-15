7ffc8e2c227cc9e5cd88061725d354b7303246a8
Jokinen Atte Samuli <attejokinen98@gmail.com>
2021-10-15 23:28:25 +0300

    Lisattiin dokumentaatiota ja muokattiin alkulukufunktio listan palauttavaksi. Lisattiin kuormitus laskemisen aloituspisteeseen.

--- PrimeNumbers/PrimeNumbers.cs
+++ PrimeNumbers/PrimeNumbers.cs
@@ -56,6 +56,7 @@ public class PrimeNumbers
 	/// Laskee alkulukuja ja listaa ne
 	/// </summary>
 	/// <param name="numbers">Kuinka monta alkulukua listataan</param>
+	/// <returns>Lista, jossa on haluttu maara alkulukuja</returns>
     public static List<int> CalculatePrimeNumbers(int numbers)
     {
         int addedNumbers = 0;
@@ -63,6 +64,47 @@ public class PrimeNumbers
 
         List<int> primeNumbers = new List<int>();
         
+        while (addedNumbers <= numbers)
+        {
+            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 11)
+            {
+                Console.WriteLine(i);
+                addedNumbers++;
+                i++;
+
+                primeNumbers.Add(i);
+
+                continue;
+            }
+
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 && i % 11 != 0)
+            {
+                Console.WriteLine(i);
+                addedNumbers++;
+
+                primeNumbers.Add(i);
+            }
+
+            i++;
+        }
+
+        return primeNumbers;
+    }
+
+
+    /// <summary>
+	/// Laskee alkulukuja ja listaa ne
+	/// </summary>
+	/// <param name="numbers">Kuinka monta alkulukua listataan</param>
+	/// <param name="startingPoint">Mista luvusta lahdetaan laskemaan</param>
+	/// <returns>Lista, jossa on haluttu maara alkulukuja</returns>
+    public static List<int> CalculatePrimeNumbers(int numbers, int startingPoint)
+    {
+        int addedNumbers = 0;
+        int i = startingPoint;
+
+        List<int> primeNumbers = new List<int>();
+
         while (addedNumbers <= numbers)
         {
             if (i == 1 || i == 3 || i == 5 || i == 7 || i == 11)
